af2770095c0b24ebc18a8976c7e7fba0b9635af1
Jordan <Jordan@DESKTOP-G3SHIT1>
2024-06-18 09:55:04 +0200

    Přidejte soubory projektu.

---
+++ NactiData/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace NactiData
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] inputs = new int[3];
+			for (int i = 0; i < 3; i++)
+			{
+				inputs[i] = GetNumber();
+			}
+            Console.WriteLine($"součet všech 3 čísel: {GetSum(inputs)}");
+            Console.WriteLine($"součin všech 3 čísel: {GetProduct(inputs)}");
+            Console.WriteLine($"součet prvních dvou čísel dělený třetím: {GetThis(inputs)}");
+
+			Console.WriteLine("\nPro ukončení programu stiskněte enter");
+			Console.ReadKey();
+        }
+
+		public static int GetNumber()
+		{
+			Console.Write("Zadejte číslo: ");
+			string value = Console.ReadLine();
+			int result = 0;
+			try
+			{
+				result = Convert.ToInt32(value);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Hodnota musí být číslo");
+				result = GetNumber();
+			}
+			return result;
+		}
+		public static int GetSum(int[] inputs)
+		{
+			int result = 0;
+			foreach (int i in inputs)
+			{
+				result += i;
+			}
+			return result;
+		}
+		public static int GetProduct(int[] inputs)
+		{
+			return inputs[0] * inputs[1] *  inputs[2];
+		}
+		public static decimal GetThis(int[] inputs)
+		{			
+			return (inputs[0] * inputs[1]) / (decimal)inputs[2];
+		}
+	}
+}
